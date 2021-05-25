e234ef8374eb0b2f22892008fdac58933132ada2
Fredson Chaves <fredsonchaves07@gmail.com>
2021-05-25 16:54:20 -0300

    feat: section 4

--- section 4/app.py
+++ section 4/app.py
@@ -1,13 +1,34 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
 api = Api(app)
 
-class Student(Resource):
-    def get(self, name):
-        return {'student': name}
+items = []
 
-api.add_resource(Student, '/student/<string:name>')
-app.run(port=5000)
+class Item(Resource):
+    def get(self, name):
+        for item in items:
+            if item['name'] == name:
+                return item
+        return {'item': None}, 404
+    
+    def post(self, name):
+        data = request.get_json(force=True)
+        
+        item = {'name': name, 'price': data['price']}
+        
+        items.append(item)
+        
+        return item, 201
+
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+api.add_resource(Item, '/item/<string:name>')
+api.add_resource(ItemList, '/items')
+
+app.run(port=5000, debug=True)
