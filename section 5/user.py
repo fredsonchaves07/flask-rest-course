6250e50065f0771d9f4689bfa3031bf3b9718a7f
Fredson Chaves <fredsonchaves07@gmail.com>
2021-05-31 18:24:49 -0300

    feat: logging application

--- section 5/user.py
+++ section 5/user.py
@@ -1,6 +1,44 @@
+import sqlite3
+from sqlite3.dbapi2 import connect
+
 class User:
     def __init__(self, _id, username, password):
         self.id = _id
         self.username = username
         self.password = password
+    
+    @classmethod
+    def find_by_username(cls, username):
+        conection = sqlite3.connect('data.db')
+        cursor = conection.cursor()
         
+        query = "SELECT * FROM users WHERE username=?"
+        
+        result = cursor.execute(query, (username,))
+        row = result.fetchone()
+        
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+        
+        conection.close()
+        return user
+            
+    @classmethod
+    def find_by_id(cls, _id):
+        conection = sqlite3.connect('data.db')
+        cursor = conection.cursor()
+        
+        query = "SELECT * FROM users WHERE id=?"
+        
+        result = cursor.execute(query, (_id,))
+        row = result.fetchone()
+        
+        if row:
+            user = cls(*row)
+        else:
+            user = None
+        
+        conection.close()
+        return user
