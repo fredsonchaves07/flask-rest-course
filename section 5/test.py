dde7383cb364efd494a0953ee75b3ae9667f76ac
Fredson Chaves <fredsonchaves07@gmail.com>
2021-05-31 18:56:21 -0300

    register user

--- section 5/test.py
+++ section 5/test.py
@@ -4,16 +4,16 @@ connection = sqlite3.connect('data.db')
 
 cursor = connection.cursor()
 
-create_table = 'CREATE TABLE users(id int, username text, password text)'
+create_table = 'CREATE TABLE users(id integer primary key, username text, password text)'
 cursor.execute(create_table)
 
-user = (1, 'jose', 'asdf')
-insert_query = 'INSERT INTO users VALUES(?, ?, ?)'
+user = ('jose', 'asdf')
+insert_query = "INSERT INTO users VALUES(NULL, ?, ?)"
 cursor.execute(insert_query, user)
 
 users = [
-    (2, 'fredson', '1234'),
-    (3, 'anne', '1234')
+    ('fredson', '1234'),
+    ('anne', '1234')
 ]
 
 cursor.executemany(insert_query, users)
--- section 5/user.py
+++ section 5/user.py
@@ -1,5 +1,5 @@
 import sqlite3
-from sqlite3.dbapi2 import connect
+from flask_restful import Resource, request, reqparse
 
 class User:
     def __init__(self, _id, username, password):
@@ -42,3 +42,33 @@ class User:
         
         conection.close()
         return user
+    
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'username',
+        type=str,
+        required=True,
+        help='This field cannot be blank'
+    )
+    
+    parser.add_argument(
+        'password',
+        type=str,
+        required=True,
+        help='This field cannot be blank'
+    )
+    
+    def post(self):
+        data = UserRegister.parser.parse_args()
+        
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        
+        query = 'INSERT INTO users VALUES(NULL, ?, ?)'
+        cursor.execute(query, (data['username'], data['password']))
+        
+        connection.commit()
+        connection.close()
+        
+        return {'message': 'User created succesfuly'}, 201
